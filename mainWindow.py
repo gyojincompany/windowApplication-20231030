f0478c0d5a99bfa187d314d6666565c191749e21
gyojincompany <luckyred@gmail.com>
2023-11-01 09:37:09 +0900

    버튼1, 버튼2 추가

--- mainWindow.py
+++ mainWindow.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
 
 class MyWin(QWidget): # QWidget 클래스를 상속받는 자식클래스를 선언
@@ -13,7 +13,23 @@ class MyWin(QWidget): # QWidget 클래스를 상속받는 자식클래스를 선
         self.setWindowIcon(QIcon("google.png")) # 아이콘 불러오기
         self.move(300, 300) # 윈도우창이 시작되는 위치 x, y
         self.resize(400, 400) # 윈도우의 크기
+        self.btn1 = QPushButton('버튼1번', self) # 버튼1 생성
+        self.btn1.move(50, 50) # 버튼1의 위치
+        self.btn1.resize(100, 50) # 버튼1의 크기
+        self.btn1.clicked.connect(self.btn1_click) # 버튼1이 클릭되었을 때 실행될 함수를 연결
+        self.btn2 = QPushButton('버튼2번', self) # 버튼2 생성
+        self.btn2.move(150, 50)  # 버튼2의 위치
+        self.btn2.resize(100, 50)  # 버튼2의 크기
+        self.btn2.clicked.connect(self.btn2_click)  # 버튼2이 클릭되었을 때 실행될 함수를 연결
         self.show() # 윈도우창을 스크린에 보여줌
+
+    def btn1_click(self): # 버튼1번이 클릭되면 실행되는 메서드
+        print("버튼1번이 클릭됨!!!!")
+
+    def btn2_click(self): # 버튼1번이 클릭되면 실행되는 메서드
+        print("버튼2번이 클릭됨!!!!")
+
+
 
 
 if __name__ == '__main__':
